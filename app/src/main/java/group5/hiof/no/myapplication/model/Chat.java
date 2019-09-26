640b8673216e3ecee42bebd67393963e36f8fc4c
Ralf Leistad <leistad.ralf@gmail.com>
2019-09-26 11:21:10 +0200

    Added variables to each model
    Some datatypes are set to 'String' due to uncertainty regarding what datatypes should be used at the moment.

--- app/src/main/java/group5/hiof/no/myapplication/model/Chat.java
+++ app/src/main/java/group5/hiof/no/myapplication/model/Chat.java
@@ -1,4 +1,44 @@
 package group5.hiof.no.myapplication.model;
 
+import java.util.UUID;
+
 public class Chat {
+
+    private UUID chatID;
+    private User sender;
+    private User receiver;
+
+
+    public Chat() {
+
+    }
+
+
+    /*
+     * Getters and setters for field variables
+     */
+
+    public UUID getChatID() {
+        return chatID;
+    }
+
+    public void setChatID(UUID chatID) {
+        this.chatID = chatID;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
 }
