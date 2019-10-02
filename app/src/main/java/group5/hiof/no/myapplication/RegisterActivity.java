79011e779587db7467f446d00941b9fdd282db59
SverreSB <sverre.sb@gmail.com>
2019-10-02 12:35:58 +0200

    Finishing activity when user clicks register. Will add logic to this onClickListner later

--- app/src/main/java/group5/hiof/no/myapplication/RegisterActivity.java
+++ app/src/main/java/group5/hiof/no/myapplication/RegisterActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,13 +16,33 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Typeface font = Typeface.createFromAsset( getAssets(), "fonts/fontawesome-webfont.ttf" );
+
         Button goBackButton = findViewById(R.id.buttonGoBack);
+        Button registerUserButton = findViewById(R.id.buttonRegisterUser);
+
+        Typeface font = Typeface.createFromAsset( getAssets(), "fonts/fontawesome-webfont.ttf" );
+
         goBackButton.setTypeface(font);
 
+        // Button for going back to previous activity. This should be returning to login screen
         goBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
+            }
+        });
+
+        /*
+            Button for registering user. Saves data given by user to database and
+            returns to previous activity(login screen)
+
+         */
+        registerUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                    Logic for saving fields to Database
+                 */
                 finish();
             }
         });
