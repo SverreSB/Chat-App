fea23deab6e8615136b08f2cca17cab261fd5610
SverreSB <sverre.sb@gmail.com>
2019-10-02 09:05:55 +0200

    Added new activity for registering user, opens activity when button is clicked in login activity

--- app/src/main/java/group5/hiof/no/myapplication/LoginActivity.java
+++ app/src/main/java/group5/hiof/no/myapplication/LoginActivity.java
@@ -2,7 +2,10 @@ package group5.hiof.no.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button registerButton = findViewById(R.id.buttonRegister);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
