1bf0f4cc86ab3931998d90a95ba53a366872538b
SverreSB <sverre.sb@gmail.com>
2019-10-02 11:02:40 +0200

    Added a button to register activity using font from font awesome, added font to assets. Button that was added is a button that takes you back to the activity(login) that sent you to the register activity. Started with some simple design for activity

--- app/src/main/java/group5/hiof/no/myapplication/RegisterActivity.java
+++ app/src/main/java/group5/hiof/no/myapplication/RegisterActivity.java
@@ -2,7 +2,11 @@ package group5.hiof.no.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Typeface font = Typeface.createFromAsset( getAssets(), "fonts/fontawesome-webfont.ttf" );
+        Button goBackButton = findViewById(R.id.buttonGoBack);
+        goBackButton.setTypeface(font);
+
+        goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
