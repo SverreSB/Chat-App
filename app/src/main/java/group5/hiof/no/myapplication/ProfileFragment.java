f88cb1938d76d7c6c02240c5a173e90c00f1b40d
SverreSB <sverre.sb@gmail.com>
2019-11-21 11:11:05 +0100

    Added functionality to delete current user. Removes user from friendlist of current friends and remove active chats from friends before deleting chat and user

--- app/src/main/java/group5/hiof/no/myapplication/ProfileFragment.java
+++ app/src/main/java/group5/hiof/no/myapplication/ProfileFragment.java
@@ -14,11 +14,27 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import group5.hiof.no.myapplication.model.Chat;
+import group5.hiof.no.myapplication.model.Message;
+import group5.hiof.no.myapplication.model.User;
 
 
 /**
@@ -32,6 +48,9 @@ public class ProfileFragment extends Fragment {
     EditText repeatPasswordField;
     Button changePasswordButton;*/
     Button signOutButton;
+    Button deleteAccountButton;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
 
     public ProfileFragment() {
@@ -44,10 +63,13 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Assigning values to Firebase variables
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
         // Assigning values to UI references
-
         signOutButton = view.findViewById(R.id.buttonSignOut);
-
+        deleteAccountButton = view.findViewById(R.id.buttonDeleteAccount);
 
         // When pressing 'Change Password' button
         /*
@@ -79,10 +101,62 @@ public class ProfileFragment extends Fragment {
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
+                mAuth.signOut();
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 startActivity(intent);
+            }
+        });
 
+        deleteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Testing delete account", Toast.LENGTH_LONG).show();
+                final CollectionReference userReference = db.collection("users");
+                final CollectionReference chatReference = db.collection("chats");
+                final String userID = mAuth.getUid();
+
+                // Validate password
+
+                // Look through friends in both sender and receiver to remove from array
+                userReference.document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            User user = task.getResult().toObject(User.class);
+                            for (final String chatID : user.getActiveChats()) {
+                                chatReference.document(chatID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()) {
+                                            Chat currentChat = task.getResult().toObject(Chat.class);
+
+                                            int indexOfCurrentUser = currentChat.getParticipants().indexOf(userID);
+                                            int indexOfFriend;
+
+                                            if (indexOfCurrentUser == 1) {
+                                                indexOfFriend = 0;
+                                            }
+                                            else {
+                                                indexOfFriend = 1;
+                                            }
+
+                                            String friendID = currentChat.getParticipants().get(indexOfFriend);
+                                            deleteFriendFromList(userID, friendID);
+                                            deleteChatFromList(chatID, friendID);
+                                            deleteMessages(chatID);
+                                            deleteChat(chatID);
+                                        }
+                                    }
+                                });
+                            }
+                            deleteUser(userID);
+                        }
+
+                    }
+                });
+                // Look through activeChats array in sender and receiver and remove chat
+
+                // Delete user
             }
         });
 
@@ -90,7 +164,50 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
-    private void reauthenticateAndUpdatePassword(String oldPassword, final String newPassword, String repeatPassword) {
+    private void deleteFriendFromList(String currentUserID, String friendID) {
+        db.collection("users").document(friendID).update("friends", FieldValue.arrayRemove(currentUserID));
+    }
+
+    private void deleteChatFromList(String chatID, String friendID) {
+        db.collection("users").document(friendID).update("activeChats", FieldValue.arrayRemove(chatID));
+    }
+
+    private void deleteMessages(final String chatID) {
+        db.collection("chats").document(chatID).collection("messages").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    List<DocumentSnapshot> documentSnapshot = task.getResult().getDocuments();
+                    for(DocumentSnapshot message : documentSnapshot) {
+                        db.collection("chats").document(chatID).collection("messages").document(message.getId()).delete();
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void deleteChat(String chatID) {
+        db.collection("chats").document(chatID).delete();
+    }
+
+    private void deleteUser(String userID) {
+        db.collection("users").document(userID).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                mAuth.getCurrentUser().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+
+
+    /*private void reauthenticateAndUpdatePassword(String oldPassword, final String newPassword, String repeatPassword) {
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String email = user.getEmail();
 
@@ -107,9 +224,9 @@ public class ProfileFragment extends Fragment {
                }
            }
         });
-    }
+    }*/
 
-    private void updatePassword(FirebaseUser user, String password) {
+    /*private void updatePassword(FirebaseUser user, String password) {
         user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -121,6 +238,5 @@ public class ProfileFragment extends Fragment {
                 }
             }
         });
-    }
-
+    }*/
 }
